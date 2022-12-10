4c5eff9d09b2643dc763031282ff4b5325dbcc02
Valeriy <lvv0490@gmail.com>
2022-12-11 00:22:19 +0200

    add retrofit

---
+++ app/src/main/java/com/example/numberfacts/api/RequestsApi.kt
@@ -0,0 +1,14 @@
+package com.example.numberfacts.api
+
+import io.reactivex.rxjava3.core.Single
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface RequestsApi {
+
+    @GET("/{number}")
+    fun getNumberInfo(@Path("number") number: Int): Single<String>
+
+    @GET("/random/math")
+    fun getRandomNumberInfo(): Single<String>
+}
--- app/src/main/java/com/example/numberfacts/di/NetworkModule.kt
+++ app/src/main/java/com/example/numberfacts/di/NetworkModule.kt
@@ -1,16 +1,19 @@
 package com.example.numberfacts.di
 
 import com.example.numberfacts.BuildConfig
+import com.example.numberfacts.api.RequestsApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.components.ViewModelComponent
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
 
 @Module
-@InstallIn(ActivityComponent::class)
+@InstallIn(ViewModelComponent::class)
 class NetworkModule {
 
     @Provides
@@ -24,8 +27,15 @@ class NetworkModule {
         return Retrofit.Builder()
             .client(okHttpClient)
             .baseUrl(BuildConfig.BASE_URL)
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+            .addConverterFactory(ScalarsConverterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+    }
+
+    @Provides
+    fun providesService(retrofit: Retrofit): RequestsApi {
+        return retrofit.create(RequestsApi::class.java)
     }
 
 }
