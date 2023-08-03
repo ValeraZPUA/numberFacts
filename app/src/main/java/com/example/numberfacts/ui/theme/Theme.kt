317a5a315cb2cc81441458f93485326ab698c13f
Valery <lvv0490@gmail.com>
2023-08-03 10:10:19 +0300

    added: MainComposeActivity

--- app/src/main/java/com/example/numberfacts/ui/theme/Theme.kt
+++ app/src/main/java/com/example/numberfacts/ui/theme/Theme.kt
@@ -38,7 +38,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun NumberFactsComposeTheme(
+fun NumberFactsTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
